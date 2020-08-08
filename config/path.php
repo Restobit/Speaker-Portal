99e58204114a05e3aa3f75018980c27a9a4e6c14
Fekete Ottó <fekete.otto@yahoo.com>
2020-08-08 19:09:03 +0200

    Fixed dynamic URL

--- config/path.php
+++ config/path.php
@@ -1,3 +1,6 @@
 <?php
+define('ABSPATH', dirname(dirname(__FILE__)) . '/');
+$ABSPATH_Array = explode("\\", ABSPATH);
+$localhostSiteName = $ABSPATH_Array[count($ABSPATH_Array) - 1];
 define('URL', 'http://localhost/speakerportal/');
 define('PAGENAME', 'Speaker Portal');
